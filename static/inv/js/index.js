589afca615d9f5c5176e395e6604127514c9b681
Jacqueline Arsenault <justcordelya@gmail.com>
2020-09-28 13:08:12 -0400

    Functional keyword filter It's not fancy (selected keywords don't toggle back off - you have to reset the whole thing).

--- static/inv/js/index.js
+++ static/inv/js/index.js
@@ -11,6 +11,47 @@ $(document).ready( function () {
 
 } );
 
+$("li.kw").click(function() {
+var keyword = $(this).attr("class").split(" ");
+keyword = keyword.filter(function(value, index, arr){ return value != "on";});
+keyword = keyword.filter(function(value, index, arr){ return value != "off";});
+keyword = keyword.filter(function(value, index, arr){ return value != "kw";});
+keyword = keyword.filter(function(value, index, arr){ return value != "neutral";});
+
+$("li." + keyword).addClass("on");
+$("li." + keyword).removeClass("neutral");
+$("li." + keyword).removeClass("off");
+
+$("li.neutral").each(function() {
+$(this).addClass("off");
+$(this).removeClass("neutral");
+$(this).removeClass("on");
+});
+
+$('div.item').not("." + keyword).each(function()
+{
+  $(this).hide();
+});
+});
+
+$("p.remove").click(function() {
+  $("li.on").each(function() {
+$(this).addClass("neutral");
+$(this).removeClass("on");
+$(this).removeClass("off");
+});
+
+$("li.off").each(function() {
+$(this).addClass("neutral");
+$(this).removeClass("off");
+$(this).removeClass("on");
+});
+
+$("div.item").each(function() {
+  $(this).show();
+});
+});
+
 function hiderows() {
   var panels = document.getElementsByClassName("full");
   for(var i = 0; i < panels.length; i++) {
